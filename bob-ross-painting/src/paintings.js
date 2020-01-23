8743e0c8c34b9682cde194d903a28c1cad9e001b
kimSpiegel04 <k.spiegel72@gmail.com>
2020-01-23 16:07:05 -0500

    created painting data

---
+++ bob-ross-painting/src/paintings.js
@@ -0,0 +1,16 @@
+export const paintings = [
+    {
+        id: 1,
+        img: '\paintings\victor-mtn-1.jpeg',
+        title: 'Mountains',
+        artist: 'Victor Prato',
+        date: 'January 11th, 2020'
+    },
+    {
+        id: 2,
+        img: '\paintings\kim-mtn-1.jpeg',
+        title: 'Mountains',
+        artist: 'Kimberly Spiegel',
+        date: 'January 11th, 2020'
+    }
+]
