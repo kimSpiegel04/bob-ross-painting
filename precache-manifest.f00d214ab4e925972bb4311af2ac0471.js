27165aae40bf479d80693211e19d91d193320422
kimSpiegel04 <k.spiegel72@gmail.com>
2020-01-27 15:45:46 -0500

    Updates

---
+++ precache-manifest.f00d214ab4e925972bb4311af2ac0471.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f044a89c7860aa785210f6148cefddfb",
+    "url": "/bob-ross-painting/index.html"
+  },
+  {
+    "revision": "70b5adbb3f90dbb71267",
+    "url": "/bob-ross-painting/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "41b2257255ea95a8aef9",
+    "url": "/bob-ross-painting/static/css/main.f0f95423.chunk.css"
+  },
+  {
+    "revision": "70b5adbb3f90dbb71267",
+    "url": "/bob-ross-painting/static/js/2.366ea78d.chunk.js"
+  },
+  {
+    "revision": "439b2841c06263adadffe7a471252397",
+    "url": "/bob-ross-painting/static/js/2.366ea78d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41b2257255ea95a8aef9",
+    "url": "/bob-ross-painting/static/js/main.ecbb6f34.chunk.js"
+  },
+  {
+    "revision": "2dfbddb5c5d8576c4761",
+    "url": "/bob-ross-painting/static/js/runtime-main.db80f7d1.js"
+  },
+  {
+    "revision": "1ff15b00d47e8f63a1eff6dba334694a",
+    "url": "/bob-ross-painting/static/media/kim-mtn-1.1ff15b00.jpeg"
+  },
+  {
+    "revision": "ba9b9a4d3aaacded9d954ae9a9817cba",
+    "url": "/bob-ross-painting/static/media/victor-mtn-1.ba9b9a4d.jpeg"
+  }
+]);
